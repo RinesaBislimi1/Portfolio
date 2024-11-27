a33262a77e46c4696ea6f0f87d6267a5190676e6
RinesaBislimi <rinesa.bislimi@student.uni-pr.edu>
2024-11-27 01:08:34 +0100

    Fix

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/Portfolio" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/services" element={<Services />} />
